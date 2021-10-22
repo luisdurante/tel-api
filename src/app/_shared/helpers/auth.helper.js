724a8615b46c8fa2871f0dca4f2983a30192a458
Luis Durante <luisdurante2@outlook.com>
2021-10-22 19:03:56 -0300

    Add more functions on auth helper

--- src/app/_shared/helpers/auth.helper.js
+++ src/app/_shared/helpers/auth.helper.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const moment = require('moment');
+const createError = require('./error-handler.helper');
 
 async function hashPassword(password) {
   const salt = await bcrypt.genSalt();
@@ -13,11 +15,42 @@ async function comparePassword(password, hashedPassword) {
 }
 
 function generateToken(id) {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: 1800 });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: 30000 });
+}
+
+function getAuthToken(authHeader) {
+  if (!authHeader) {
+    throw createError(401, 'Não autorizado');
+  }
+
+  const token = authHeader.split('Bearer ')[1] || null;
+
+  if (!token) {
+    throw createError(401, 'Não autorizado');
+  }
+
+  return token;
+}
+
+function verifyToken(reqToken) {
+  try {
+    jwt.verify(reqToken, process.env.JWT_SECRET);
+  } catch (jwtError) {
+    throw createError(401, 'Não autorizado');
+  }
+}
+
+function validateSessionTime(lastLogin, minutes) {
+  const startDate = moment().subtract(minutes, 'm').toISOString();
+  const endDate = moment().toISOString();
+  return moment(lastLogin).isBetween(startDate, endDate);
 }
 
 module.exports = {
   hashPassword,
   comparePassword,
   generateToken,
+  getAuthToken,
+  verifyToken,
+  validateSessionTime,
 };
