6f99ac69ba8852a670bcbf3b5522385880994d13
Luis Durante <luisdurante2@outlook.com>
2021-10-22 19:04:16 -0300

    Update GetUserById controller

--- src/app/controllers/users.controller.js
+++ src/app/controllers/users.controller.js
@@ -1,11 +1,34 @@
 const usersService = require('../services/users.service');
 const signupHelper = require('../_shared/helpers/signup.helper');
 const signinHelper = require('../_shared/helpers/signin.helper');
+const authHelper = require('../_shared/helpers/auth.helper');
 
 class UsersController {
   async list(req, res) {
     const users = await usersService.list();
     return res.json({ data: users });
+  }
+
+  async getById(req, res) {
+    try {
+      const userId = req.params.id;
+      const token = authHelper.getAuthToken(req.headers.authorization);
+      authHelper.verifyToken(token);
+
+      const user = await usersService.getById(userId);
+
+      if (token !== user.token) {
+        return res.status(401).json({ mensagem: 'Não autorizado' });
+      }
+
+      if (!authHelper.validateSessionTime(user.ultimo_login, 30)) {
+        return res.status(401).json({ mensagem: 'Sessão inválida' });
+      }
+
+      return res.status(200).json(user);
+    } catch (err) {
+      return res.status(err.status || 500).json({ mensagem: err.message });
+    }
   }
 
   async signup(req, res) {
