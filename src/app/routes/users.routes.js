4b600a0026373fa99b3172e4930a648e8b6915b0
Luis Durante <luisdurante2@outlook.com>
2021-10-22 21:57:57 -0300

    Change signup route to just /users

--- src/app/routes/users.routes.js
+++ src/app/routes/users.routes.js
@@ -1,12 +1,12 @@
 const routes = require('express').Router();
 const usersController = require('../controllers/users.controller');
 
-routes.post('/users/signup', usersController.signup);
+routes.post('/users', usersController.signup);
 
 routes.post('/users/signin', usersController.signin);
 
 routes.get('/users/:id', usersController.getById);
 
-routes.get('/users', usersController.list);
+routes.get('/users/list', usersController.list);
 
 module.exports = routes;
