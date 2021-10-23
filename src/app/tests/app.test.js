29fd395cc9a4f6749aac522fb1ea22edeb9d1d42
Luis Durante <luisdurante2@outlook.com>
2021-10-22 21:58:35 -0300

    Add Jest

---
+++ src/app/tests/app.test.js
@@ -0,0 +1,76 @@
+const app = require('../../server');
+const mongodb = require('../_shared/config/mongodb.config');
+const request = require('supertest');
+
+describe('POST /users', () => {
+  describe('Given email and password', () => {
+    it('Should return 201 Created - with user object', async () => {
+      const response = await request(app)
+        .post('/users')
+        .send({
+          nome: 'User Test',
+          email: 'usuariotest@hotmail.com',
+          senha: '12345',
+          telefones: [
+            {
+              numero: '12342342',
+              ddd: '13',
+            },
+            {
+              numero: '132333342',
+              ddd: '11',
+            },
+          ],
+        });
+
+      expect(response.statusCode).toBe(201);
+    });
+
+    it('Should return 409 Conflict - Email is already in use', async () => {
+      const response = await request(app).post('/users').send({
+        email: 'usuariotest@hotmail.com',
+        senha: '12345',
+      });
+
+      expect(response.statusCode).toBe(409);
+    });
+  });
+
+  describe('Given just the password', () => {
+    it('Should return 400 Bad Request - Email must be sent', async () => {
+      const response = await request(app).post('/users').send({
+        senha: '12345',
+      });
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toStrictEqual({
+        mensagem: 'Email deve ser enviado para o cadastro',
+      });
+    });
+  });
+});
+
+describe('POST /users/signin', () => {
+  it('Should return 200 OK - email and password matches', async () => {
+    const response = await request(app).post('/users/signin').send({
+      email: 'usuariotest@hotmail.com',
+      senha: '12345',
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('Should return 401 Unauthorized - Email or password are wrong', async () => {
+    const response = await request(app).post('/users/signin').send({
+      senha: '12345',
+    });
+
+    expect(response.statusCode).toBe(401);
+  });
+});
+
+afterAll(async () => {
+  const db = await mongodb.connect();
+  await db.collection('users').deleteOne({ email: 'usuariotest@hotmail.com' });
+  await mongodb.close();
+});
