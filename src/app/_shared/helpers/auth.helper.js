cdab93e81beed2f1e7a2aa7db0623e2c7dd9a6ce
Luis Durante <luisdurante2@outlook.com>
2021-10-24 15:14:58 -0300

    Update session time to 30 minutes

--- src/app/_shared/helpers/auth.helper.js
+++ src/app/_shared/helpers/auth.helper.js
@@ -15,7 +15,7 @@ async function comparePassword(password, hashedPassword) {
 }
 
 function generateToken(id) {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: 30000 });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: 1800 });
 }
 
 function getAuthToken(authHeader) {
